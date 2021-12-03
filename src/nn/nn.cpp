21067d216f5382f7b30b3d95ade50d9c97000e31
adamcihlar <adam.cihlar3@gmail.com>
2021-12-03 14:25:02 +0100

    Omp 8

--- src/nn/nn.cpp
+++ src/nn/nn.cpp
@@ -192,11 +192,12 @@ public:
 	* Randomly changes order of input vectors and labels inplace while preserving the correct matching.
 	*/
 	void shuffle() {
-		std::vector<int> indexes; // ALLOC - dims known on init of Dataset
-		indexes.reserve(X.size());
+		std::vector<int> indexes (X.size()); // ALLOC - dims known on init of Dataset
+		//indexes.reserve(X.size());
 #pragma omp parallel for num_threads(NUM_THREADS)
 		for (int i = 0; i < X.size(); ++i)
-			indexes.push_back(i);
+			//indexes.push_back(i);
+			indexes[i];
 		std::random_shuffle(indexes.begin(), indexes.end());
 
 		std::vector<std::vector<double>> X_shuffled(X_rows, std::vector<double>(X_cols)); // ALLOC
